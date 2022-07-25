97befe367962c25ad4c68512d6830f139f6be269
William Moraes <willmoraes77@gmail.com>
2022-07-25 16:09:32 -0300

    updates

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,27 +1,21 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import { StatusBar } from 'react-native';
-import { RFValue } from 'react-native-responsive-fontsize';
-import { useNetInfo } from '@react-native-community/netinfo';
-import { synchronize } from '@nozbe/watermelondb/sync';
+import React, {useCallback, useEffect, useRef, useState} from 'react';
+import {useFocusEffect, useNavigation} from '@react-navigation/native';
+import {StatusBar} from 'react-native';
+import {RFValue} from 'react-native-responsive-fontsize';
+import {useNetInfo} from '@react-native-community/netinfo';
+import {synchronize} from '@nozbe/watermelondb/sync';
 
-import { database } from '../../database';
-import { api } from '../../services/api';
+import {database} from '../../database';
+import {api} from '../../services/api';
 
 import Logo from '../../assets/logo.svg';
-import { CarDTO } from '../../dtos/CarDTO';
+import {CarDTO} from '../../dtos/CarDTO';
 
-import { Car } from '../../components/Car';
-import { Car as ModelCar } from '../../database/model/Car';
-import { LoadAnimation } from '../../components/LoadAnimation';
+import {Car} from '../../components/Car';
+import {Car as ModelCar} from '../../database/model/Car';
+import {LoadAnimation} from '../../components/LoadAnimation';
 
-import {
-  Container,
-  Header,
-  HeaderContent,
-  TotalCars,
-  CarList,
-} from './styles';
+import {Container, Header, HeaderContent, TotalCars, CarList} from './styles';
 
 export function Home() {
   const [cars, setCars] = useState<ModelCar[]>([]);
@@ -32,21 +26,22 @@ export function Home() {
   const synchronizing = useRef(false);
 
   function handleCarDetails(car: CarDTO) {
-    navigation.navigate('CarDetails', { carId: car.id })
+    navigation.navigate('CarDetails', {carId: car.id});
   }
 
   async function offlineSynchronize() {
     await synchronize({
       database,
-      pullChanges: async ({ lastPulledAt }) => {
-        const response = await api
-          .get(`cars/sync/pull?lastPulledVersion=${lastPulledAt || 0}`);
+      pullChanges: async ({lastPulledAt}) => {
+        const response = await api.get(
+          `cars/sync/pull?lastPulledVersion=${lastPulledAt || 0}`,
+        );
 
-        const { changes, latestVersion } = response.data;
+        const {changes, latestVersion} = response.data;
 
-        return { changes, timestamp: latestVersion }
+        return {changes, timestamp: latestVersion};
       },
-      pushChanges: async ({ changes }) => {
+      pushChanges: async ({changes}) => {
         const user = changes.users;
         if (user) {
           await api.post('/users/sync', user);
@@ -57,14 +52,12 @@ export function Home() {
     await fetchCars();
   }
 
-
   async function fetchCars() {
     try {
       const carCollection = database.get<ModelCar>('cars');
       const cars = await carCollection.query().fetch();
 
       setCars(cars);
-
     } catch (error) {
       console.log(error);
     } finally {
@@ -84,28 +77,25 @@ export function Home() {
     };
   }, []);
 
+  useFocusEffect(
+    useCallback(() => {
+      const syncChanges = async () => {
+        if (netInfo.isConnected && !synchronizing.current) {
+          synchronizing.current = true;
 
-  useFocusEffect(useCallback(() => {
-    const syncChanges = async () => {
-      if (netInfo.isConnected && !synchronizing.current) {
-        synchronizing.current = true;
-
-        try {
-          await offlineSynchronize();
+          try {
+            await offlineSynchronize();
+          } catch (err) {
+            console.log(err);
+          } finally {
+            synchronizing.current = false;
+          }
         }
-        catch (err) {
-          console.log(err);
-        }
-        finally {
-          synchronizing.current = false;
-        }
-      }
-    }
+      };
 
-    syncChanges();
-  }, [netInfo.isConnected]));
-
-
+      syncChanges();
+    }, [netInfo.isConnected]),
+  );
 
   return (
     <Container>
@@ -116,28 +106,22 @@ export function Home() {
       />
       <Header>
         <HeaderContent>
-          <Logo
-            width={RFValue(108)}
-            height={RFValue(12)}
-          />
-          {
-            !loading &&
-            <TotalCars>
-              Total de {cars.length} carros
-            </TotalCars>
-          }
+          <Logo width={RFValue(108)} height={RFValue(12)} />
+          {!loading && <TotalCars>Total de {cars.length} carros</TotalCars>}
         </HeaderContent>
       </Header>
 
-      {loading ? <LoadAnimation /> :
+      {loading ? (
+        <LoadAnimation />
+      ) : (
         <CarList
           data={cars}
           keyExtractor={item => item.id}
-          renderItem={({ item }) =>
+          renderItem={({item}) => (
             <Car data={item} onPress={() => handleCarDetails(item)} />
-          }
+          )}
         />
-      }
+      )}
     </Container>
   );
 }
